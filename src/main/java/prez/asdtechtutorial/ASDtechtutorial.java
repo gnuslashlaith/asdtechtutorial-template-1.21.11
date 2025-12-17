7e879341475ce6e230211705cca08c0d39bd40b4
Laith <lake.ghandour@gmail.com>
2025-12-17 20:22:27 +0300

    super second commit

--- src/main/java/prez/asdtechtutorial/ASDtechtutorial.java
+++ src/main/java/prez/asdtechtutorial/ASDtechtutorial.java
@@ -14,6 +14,6 @@ public class ASDtechtutorial implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("hello my frien");
 	}
 }
